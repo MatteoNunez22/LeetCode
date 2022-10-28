a4b9b5ba32369d438dbac4621ac2e7de277b40d1
Matteo Nunez <matteo.nunez22@gmail.com>
2022-10-28 03:10:10 -0400

    Time: 107 ms (38.73%), Space: 14 MB (20.28%) - LeetHub

---
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
@@ -0,0 +1,32 @@
+class Solution:
+    def shortestWay(self, source: str, target: str) -> int:
+        order = {}
+        for i in range(len(source)):
+            if source[i] in order:
+                order[source[i]].append(i)
+            else:
+                order[source[i]] = [i]
+            
+        def getNextIndex(array, value):
+            for k in range(len(array)):
+                if array[k] > value:
+                    return array[k]
+            else:
+                return -1
+          
+        idx = -1
+        count = 1
+        for j in range(len(target)):
+            if target[j] not in source:
+                return -1
+            nextIdx = getNextIndex(order[target[j]], idx)
+            if not nextIdx == -1:
+                idx = nextIdx
+            else:
+                count += 1
+                idx = order[target[j]][0]
+            
+        return count
+            
+            
+            
