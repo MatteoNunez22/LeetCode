75e94bdd999f05dfe4e01ec6c53ea6d7da1a6beb
Matteo Nunez <matteo.nunez22@gmail.com>
2022-10-30 14:28:49 -0400

    Time: 380 ms (54.17%), Space: 17.4 MB (5.46%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.py
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.py
@@ -2,20 +2,27 @@ class Solution:
     def numSplits(self, s: str) -> int:
         left = set()
         num_left = []
+        # for i in range(len(s)-1):
+        #     if not s[i] in left:
+        #         left.add(s[i])
+        #     num_left.append(len(left))
+        
+        right = set()
+        num_right = []
+        # for i in range(len(s)-1,0,-1):
+        #     if not s[i] in right:
+        #         right.add(s[i])
+        #     num_right.append(len(right))
+            
         for i in range(len(s)-1):
             if not s[i] in left:
                 left.add(s[i])
             num_left.append(len(left))
-        
-        right = set()
-        num_right = []
-        for i in range(len(s)-1,0,-1):
-            if not s[i] in right:
-                right.add(s[i])
+            if not s[len(s)-1-i] in right:
+                right.add(s[len(s)-1-i])
             num_right.append(len(right))
         
         num_right = num_right[::-1]
-        
         good_splits = 0
         for i in range(len(s)-1):
             if num_right[i] == num_left[i]:
