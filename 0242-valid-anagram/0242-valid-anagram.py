656643334a0701a603966faa036ab9dde78ce466
Matteo Nunez <matteo.nunez22@gmail.com>
2023-01-03 20:08:52 -0500

    Time: 160 ms (7.78%), Space: 14.4 MB (66.16%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,10 +2,29 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-
-        s = list(s)
-        t = list(t)
-        s.sort()
-        t.sort()
-
-        return s == t
+        
+        # Approach 1: O(nlogn)
+        # s = list(s)
+        # t = list(t)
+        # s.sort()
+        # t.sort()
+        # return s == t
+    
+        # Approach 2: O(n)
+        hashmap = {}
+        for i in range(len(t)):
+            if t[i] in hashmap:
+                hashmap[t[i]] += 1
+            else:
+                hashmap[t[i]] = 1
+            if s[i] in hashmap:
+                hashmap[s[i]] -= 1
+            else:
+                hashmap[s[i]] = -1
+        for char in hashmap:
+            if hashmap[char] != 0:
+                return False
+        return True
+            
+        
+            
