c25d6842c3b148246af79ce60ee69d5b6e8bd841
Matteo Nunez <matteo.nunez22@gmail.com>
2023-01-04 21:42:32 -0500

    Time: 159 ms (54.30%), Space: 20.1 MB (5.84%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -5,20 +5,34 @@ class Solution:
         
         count = {}
         for num in nums:
-            count[num] = count.get(num, 0) + 1
-           
+            count[num] = count.get(num, 0) + 1       
         print(count)
-        maxheap = []
-        heapq.heapify(maxheap)
-        for n, c in count.items():
-            heapq.heappush(maxheap, (-c,n))
-            
-        answer = []
-        for i in range(k):
-            pop = heapq.heappop(maxheap)
-            print(pop)
-            answer.append(pop[1])
         
+        # Approach 1: O(klogN)
+        # maxheap = []
+        # heapq.heapify(maxheap)
+        # for n, c in count.items():
+        #     heapq.heappush(maxheap, (-c,n))
+        # answer = []
+        # for i in range(k):
+        #     pop = heapq.heappop(maxheap)
+        #     print(pop)
+        #     answer.append(pop[1])
+        # return answer
+    
+        # Approach 2: O(N)
+        bucket = [[] for i in range(len(nums)+1)]
+        for n, c in count.items():
+            bucket[c].append(n)
+        print(bucket)
+        
+        answer = []
+        for n_list in bucket[::-1]:
+            if n_list:
+                for n in n_list:
+                    answer.append(n)
+                    if len(answer) == k:
+                        return answer
         return answer
     
         
