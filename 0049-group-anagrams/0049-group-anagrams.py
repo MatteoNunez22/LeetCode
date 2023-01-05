c027b2b6bb06bbbeca51e7734f4058464eaa0ae8
Matteo Nunez <matteo.nunez22@gmail.com>
2023-01-04 19:13:40 -0500

    Time: 352 ms (7.01%), Space: 19.7 MB (28.67%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,14 +1,28 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Approach 1
+        # hashmap = {}
+        # for word in strs:
+        #     word_list = list(word)
+        #     word_list.sort()
+        #     key = ''.join(word_list)
+        #     if key in hashmap:
+        #         hashmap[key] = hashmap[key] + [word]
+        #     else:
+        #         hashmap[key] = [word]
+        # return hashmap.values()
+        
+        # Approach 2
         hashmap = {}
         for word in strs:
-            word_list = list(word)
-            word_list.sort()
-            key = ''.join(word_list)
-            
+            count = [0] * 26
+            for c in word:
+                count[ord(c) - ord('a')] += 1
+                
+            key = tuple(count)
             if key in hashmap:
                 hashmap[key] = hashmap[key] + [word]
             else:
                 hashmap[key] = [word]
-        
         return hashmap.values()
+                
