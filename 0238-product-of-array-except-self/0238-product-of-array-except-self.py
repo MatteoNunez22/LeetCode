f3569dfa147e2dad1473845968fb63a6c74608ff
Matteo Nunez <matteo.nunez22@gmail.com>
2023-01-05 16:37:29 -0500

    Time: 692 ms (16.69%), Space: 21.3 MB (55.66%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,22 +1,36 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # Approach 1: O(N)
+        # N = len(nums)
+        # left = [1] * (N)
+        # prefix = 1
+        # for i in range(1, N):
+        #     left[i] = prefix * nums[i-1]
+        #     prefix = left[i]
+        # print(left)
+        # right = [1] * (N)
+        # suffix = 1
+        # for i in range(N-2, -1, -1):
+        #     right[i] = suffix * nums[i+1]
+        #     suffix = right[i]
+        # print(right)
+        # output = [1] * N
+        # for i in range(0, N):
+        #     output[i] = left[i] * right[i]
+        # return output
+        
+        # Approach 2: O(N)
         N = len(nums)
-        left = [1] * (N)
-        prefix = 1
-        for i in range(1, N):
-            left[i] = prefix * nums[i-1]
-            prefix = left[i]
-        print(left)
-            
-        right = [1] * (N)
-        suffix = 1
-        for i in range(N-2, -1, -1):
-            right[i] = suffix * nums[i+1]
-            suffix = right[i]
-        print(right)
         
         output = [1] * N
-        for i in range(0, N):
-            output[i] = left[i] * right[i]
+        prefix = 1
+        for i in range(1, N):
+            output[i] = prefix * nums[i-1]
+            prefix = output[i]
         
+        suffix = 1
+        for i in range(N-2, -1, -1):
+            output[i] *= suffix * nums[i+1]
+            suffix = suffix * nums[i+1]
+            
         return output
