e7ca94a9b9415a42151a490321532776b811032d
Matteo Nunez <matteo.nunez22@gmail.com>
2023-01-06 17:36:56 -0500

    Time: 75 ms (72.27%), Space: 14.4 MB (51.41%) - LeetHub

---
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -0,0 +1,33 @@
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        encoded_string = ""
+        for string in strs:
+            n = len(string)
+            encoded_string = encoded_string + str(n) + '#' + string
+        print(encoded_string)
+        return encoded_string
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        strs = []
+        idx = 0
+        while idx < len(s):
+            n = ""
+            while s[idx] != "#":
+                n = n + s[idx]
+                idx += 1
+            idx += 1 
+            num = int(n)
+            print(idx, num)
+            strs.append(s[idx:idx+num])
+            idx += num
+        
+        return strs
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
