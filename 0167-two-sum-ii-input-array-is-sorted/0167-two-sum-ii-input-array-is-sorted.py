b1f3511bf6afc654cea5e07ab155a912f8a56278
Matteo Nunez <matteo.nunez22@gmail.com>
2023-01-17 22:49:02 -0500

    Time: 144 ms (52.12%), Space: 14.9 MB (38.36%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -4,10 +4,9 @@ class Solution:
         r = len(numbers) - 1
         
         while l<r:
-            twosum = numbers[l] + numbers[r]
-            if twosum == target:
+            if numbers[l] + numbers[r] == target:
                 return [l+1, r+1]
-            elif twosum < target:
+            elif numbers[l] + numbers[r] < target:
                 l += 1
             else:
                 r -= 1
