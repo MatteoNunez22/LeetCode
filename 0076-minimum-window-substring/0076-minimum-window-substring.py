fbc4ddc889261d60298d335f244039bd3e82f6c8
Matteo Nunez <matteo.nunez22@gmail.com>
2023-01-22 21:59:59 -0500

    Time: 428 ms (29.21%), Space: 14.7 MB (79.02%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -15,20 +15,16 @@ class Solution:
             
         l = 0
         for r in range(len(s)):
-            # print(f"sub: {s[l:r+1]}")
             # Complete substring
             sCount[s[r]] = sCount.get(s[r], 0) + 1
-            if s[r] in t and sCount[s[r]] == tCount[s[r]]:
+            if s[r] in tCount and sCount[s[r]] == tCount[s[r]]:
                 matches += 1
-            # print(f"matches: {matches}")
             if matches == len(tCount):
-                # print("COMPLETE")
                 # Minimize substring
-                while s[l] not in t or sCount[s[l]] > tCount[s[l]]:
-                    if s[l] in t:
+                while s[l] not in tCount or sCount[s[l]] > tCount[s[l]]:
+                    if s[l] in tCount:
                         sCount[s[l]] -= 1
                     l += 1
-                # print(f"reduced to: {s[l:r+1]}")
 
                 # Check substring length
                 if len(s[l:r+1]) <= minLength:
