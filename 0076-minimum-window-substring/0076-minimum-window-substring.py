c7a116f300bf152debd5f40cd87096d9036f0218
Matteo Nunez <matteo.nunez22@gmail.com>
2023-01-22 22:15:32 -0500

    Time: 569 ms (21.02%), Space: 14.7 MB (79.02%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -16,21 +16,17 @@ class Solution:
             sCount[s[r]] = sCount.get(s[r], 0) + 1
             if s[r] in t and sCount[s[r]] == tCount[s[r]]:
                 matches += 1
-            if matches == len(tCount):
-                # Minimize substring
-                while s[l] not in t or sCount[s[l]] > tCount[s[l]]:
-                    if s[l] in t:
-                        sCount[s[l]] -= 1
-                    l += 1
-
+            # Minimize substring
+            while matches == len(tCount):
                 # Check substring length
                 if (r - l + 1) <= minLength:
                     minLength = (r - l + 1)
                     res = s[l:r+1]
-            
+                
                 # Move pointers
                 sCount[s[l]] -= 1
+                if s[l] in t and sCount[s[l]] < tCount[s[l]]:
+                    matches -= 1
                 l += 1
-                matches -= 1
         
         return res
