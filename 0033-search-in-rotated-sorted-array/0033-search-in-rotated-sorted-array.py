5120f3a282c989bb755f454d4aab08653e0b3f81
Matteo Nunez <matteo.nunez22@gmail.com>
2023-01-25 20:29:03 -0500

    Time: 47 ms (56.97%), Space: 14.3 MB (50.93%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -4,20 +4,29 @@ class Solution:
         pivot = r
 
         while l <= r:
-            mid = l + (r - l) // 2
-            if nums[mid] == target:
-                return mid
-            # Left portion is sorted
-            if nums[l] <= nums[mid]:
-                if nums[l] <= target <= nums[mid]:
+            if nums[l] < nums[r]:
+                mid = l + (r - l) // 2
+                if nums[mid] < target:
+                    l = mid + 1
+                elif nums[mid] > target:
                     r = mid - 1
                 else:
-                    l = mid + 1
-            # Right portion is sorted
+                    return mid
             else:
-                if nums[mid] <= target <= nums[r]:
-                    l = mid + 1
+                mid = l + (r - l) // 2
+                if nums[mid] == target:
+                    return mid
+                # Left portion is sorted
+                if nums[l] <= nums[mid]:
+                    if nums[l] <= target <= nums[mid]:
+                        r = mid - 1
+                    else:
+                        l = mid + 1
+                # Right portion is sorted
                 else:
-                    r = mid - 1
+                    if nums[mid] <= target <= nums[r]:
+                        l = mid + 1
+                    else:
+                        r = mid - 1
         return -1
             
