ec096af014373e2e6d99c65d59a14a101887c69c
Matteo Nunez <matteo.nunez22@gmail.com>
2023-01-25 20:27:30 -0500

    Time: 101 ms (5.49%), Space: 14.2 MB (50.93%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -16,17 +16,17 @@ class Solution:
                 mid = l + (r - l) // 2
                 if nums[mid] == target:
                     return mid
-                
+                # Left portion is sorted
                 if nums[l] <= nums[mid]:
-                    if not (nums[l] <= target <= nums[mid]):
-                        l = mid + 1
-                    else:
+                    if nums[l] <= target <= nums[mid]:
                         r = mid - 1
+                    else:
+                        l = mid + 1
+                # Right portion is sorted
                 else:
-                    if not (nums[mid] <= target <= nums[r]):
-                        r = mid - 1
-                    else:
+                    if nums[mid] <= target <= nums[r]:
                         l = mid + 1
-
+                    else:
+                        r = mid - 1
         return -1
             
