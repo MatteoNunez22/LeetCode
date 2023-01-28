c45295ff8fa6f8284da3fec8bd7ffcd609253b02
Matteo Nunez <matteo.nunez22@gmail.com>
2023-01-27 19:18:39 -0500

    Time: 41 ms (64.19%), Space: 13.8 MB (98.75%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -15,12 +15,11 @@ class Solution:
         while list1 and list2:
             if list1.val <= list2.val:
                 prev.next = list1
-                prev = list1
                 list1 = list1.next
             else:
                 prev.next = list2
-                prev = list2
                 list2 = list2.next
+            prev = prev.next
         
         if list1 is None:
             prev.next = list2 
