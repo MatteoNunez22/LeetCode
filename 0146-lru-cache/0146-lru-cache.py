0417b1d64226f21dfcfffa91d39d3ef22bd766fc
Matteo Nunez <matteo.nunez22@gmail.com>
2023-01-29 02:04:12 -0500

    Time: 929 ms (60.12%), Space: 76.6 MB (10.37%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,28 +1,48 @@
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.keys = []
         self.map = {}
+        
+        self.start = Node(0, 0)
+        self.end = Node(0, 0)
+        self.start.next = self.end
+        self.end.prev = self.start
+        
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        
+    def append(self, node):
+        self.end.prev.next = node
+        node.prev = self.end.prev
+        self.end.prev = node
+        node.next = self.end
         
     def get(self, key: int) -> int:
         if key in self.map:
-            self.keys.remove(key)
-            self.keys.append(key)
-            return self.map[key]
-        else:
-            return -1
+            self.remove(self.map[key])
+            self.append(self.map[key])
+            return self.map[key].val
+        return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.map:
-            self.keys.remove(key)
-        elif len(self.keys) == self.capacity:
-            self.map.pop(self.keys[0])
-            self.keys.remove(self.keys[0])
-        self.keys.append(key)
-        self.map[key] = value
+            self.remove(self.map[key])
+        self.map[key] = Node(key, value)
+        self.append(self.map[key])
         
-
+        if len(self.map) > self.capacity:
+            self.map.pop(self.start.next.key)
+            self.remove(self.start.next)
+        
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
