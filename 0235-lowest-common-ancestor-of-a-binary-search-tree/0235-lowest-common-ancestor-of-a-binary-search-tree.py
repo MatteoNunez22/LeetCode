f05dacdf99efbacb276bf3e7822efb7c8cc7daa2
Matteo Nunez <matteo.nunez22@gmail.com>
2023-02-02 15:20:33 -0500

    Time: 78 ms (84.00%), Space: 18.8 MB (19.11%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,14 +7,24 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if root.val == p.val or root.val == q.val:
-            return root
+        # Recursively
+#         if root.val == p.val or root.val == q.val:
+#             return root
+#         if root.val < p.val and root.val < q.val:
+#             return self.lowestCommonAncestor(root.right, p, q)
         
-        if root.val < p.val and root.val < q.val:
-            return self.lowestCommonAncestor(root.right, p, q)
+#         elif root.val > p.val and root.val > q.val:
+#             return self.lowestCommonAncestor(root.left, p, q)
+#         else:
+#             return root
         
-        elif root.val > p.val and root.val > q.val:
-            return self.lowestCommonAncestor(root.left, p, q)
+        # Iteratively
+        curr = root
         
-        else:
-            return root
+        while curr:
+            if curr.val < p.val and curr.val < q.val:
+                curr = curr.right
+            elif curr.val > p.val and curr.val > q.val:
+                curr = curr.left
+            else:
+                return curr
