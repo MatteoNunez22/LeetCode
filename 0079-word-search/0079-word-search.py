5aa7be87ee996b1041d01f70557be2af92503be1
Matteo Nunez <matteo.nunez22@gmail.com>
2023-02-03 20:03:03 -0500

    Time: 8335 ms (33.23%), Space: 13.9 MB (91.47%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,26 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        ROWS, COLS = len(board), len(board[0])
+        path = set()
+        
+        def search(r, c, i):
+            if i == len(word):
+                return True
+            
+            if ((r < 0 or r >= ROWS) or (c < 0 or c >= COLS) or 
+                (board[r][c] != word[i]) or ((r,c) in path)):
+                return False
+            
+            path.add((r,c))
+            res = (search(r + 1, c, i + 1) or search(r - 1, c, i + 1) or
+                    search(r, c + 1, i + 1) or search(r , c - 1, i + 1))
+            path.remove((r,c))
+            
+            return res
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if search(r, c, 0):
+                    return True
+                
+        return False
