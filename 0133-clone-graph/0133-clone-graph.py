a1d9a1a2655d45678f63fb3ce923df05e6d87009
Matteo Nunez <matteo.nunez22@gmail.com>
2023-02-05 19:19:03 -0500

    Time: 45 ms (46.94%), Space: 14.3 MB (70.59%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,36 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return None
+        
+        copyMap = {}
+        
+        def build(node):
+            if node.val in copyMap:
+                return copyMap[node.val]
+            
+            copy = Node(node.val)
+            copyMap[node.val] = copy
+            
+            for n in node.neighbors:
+                neighbor = build(n)
+                copy.neighbors.append(neighbor)
+                
+            return copy
+                   
+        return build(node)
+                    
+        
+        
+        
+        
+        
+        
