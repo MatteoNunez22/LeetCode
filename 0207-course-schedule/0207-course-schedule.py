4293f77391245c994b2cd7a5d3ca1b54293a181f
Matteo Nunez <matteo.nunez22@gmail.com>
2023-02-05 21:22:04 -0500

    Time: 110 ms (43.65%), Space: 17.7 MB (6.61%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if len(prerequisites) == 0:
+            return True
+        
+        reqMap = collections.defaultdict(list)
+        
+        for a, b in prerequisites:
+            reqMap[a].append(b)
+            
+        visitSet = set()
+            
+        def complete(course):
+            if course in visitSet:
+                return False
+            
+            if not reqMap[course]:
+                return True
+            
+            visitSet.add(course)
+            for i, pre in enumerate(reqMap[course]):
+                if not complete(pre):
+                    return False
+                else:
+                    reqMap[course].pop(i)
+            visitSet.remove(course)
+                    
+            return True
+        
+        for i in range(numCourses):
+            if not complete(i):
+                return False
+        
+        return True
