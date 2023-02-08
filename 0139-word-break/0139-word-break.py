11c8d91683bf7481b8b67f1774edd1bdb18fdf08
Matteo Nunez <matteo.nunez22@gmail.com>
2023-02-08 18:10:05 -0500

    Time: 36 ms (87.13%), Space: 14.1 MB (42.73%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,22 +1,34 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # Recursive DP: O(n*m*n), O(n)
+        # n = len(s)
+        # dp = []
+        # def dfs(idx):
+        #     if idx in dp:
+        #         return False
+        #     if idx == n:
+        #         return True
+        #     for word in wordDict:
+        #         l = len(word)
+        #         if idx + l <= n and s[idx:idx+l] == word:
+        #             if dfs(idx+l):
+        #                 return True
+        #     dp.append(idx)
+        #     return False
+        # return dfs(0)
+    
+        # Iterative DP: O(n*m*n), O(n)
         n = len(s)
+        dp = [True] * (n+1)
         
-        dp = []
-        
-        def dfs(idx):
-            if idx in dp:
-                return False
-            
-            if idx == n:
-                return True
-            
+        for i in range(n-1, -1, -1):
             for word in wordDict:
                 l = len(word)
-                if idx + l <= n and s[idx:idx+l] == word:
-                    if dfs(idx+l):
-                        return True
-            dp.append(idx)
-            return False
+                if i + l <= n and s[i:i+l] == word and dp[i+l]:
+                    dp[i] = True
+                    break
+                dp[i] = False
+        print(dp)
+            
+        return dp[0]
         
-        return dfs(0)
