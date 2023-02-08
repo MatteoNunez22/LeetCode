7f552337ed52f1c1a6408c0e9508a9bbe662ca1b
Matteo Nunez <matteo.nunez22@gmail.com>
2023-02-08 01:39:11 -0500

    Time: 35 ms (63.09%), Space: 13.8 MB (97.66%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,9 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
-        n = len(nums)
-        
-        if n == 1:
+        if len(nums) == 1:
             return nums[0]
         
         def houseRobber(numbers):
