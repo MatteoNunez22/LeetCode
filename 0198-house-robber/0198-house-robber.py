1aa6bdab5c02a5ed397d2f0a3b09498f13d804bd
Matteo Nunez <matteo.nunez22@gmail.com>
2023-02-08 01:04:46 -0500

    Time: 34 ms (64.30%), Space: 13.8 MB (56.37%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,19 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-    
-        if n == 1:
-            return nums[0]
+        loot1, loot2 = 0, 0
         
-        if n == 2:
-            return max(nums[0], nums[1])
-        
-        loot1, loot2 = nums[0], max(nums[0], nums[1])
-        
-        for i in range(2, n):
-            maxLoot = max(nums[i] + loot1, loot2)
+        for n in nums:
+            maxLoot = max(n + loot1, loot2)
             loot1 = loot2
             loot2 = maxLoot
         
-        return maxLoot
+        return loot2
     
