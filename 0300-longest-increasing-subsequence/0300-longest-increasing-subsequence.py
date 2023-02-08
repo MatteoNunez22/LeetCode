3cc143cb4e0bdc52465639befcdb0273bc3b33f0
Matteo Nunez <matteo.nunez22@gmail.com>
2023-02-08 18:52:48 -0500

    Time: 2195 ms (69.38%), Space: 14.1 MB (80.74%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -9,9 +9,8 @@ class Solution:
         for i in range(n-2, -1, -1):
             maxLength = 0
             for j in range(i+1, n):
-                if nums[i] < nums[j]:
-                    if dp[j] + 1 > maxLength:
-                        maxLength = dp[j]
+                if nums[i] < nums[j] and dp[j] + 1 > maxLength:
+                    maxLength = dp[j]
             dp[i] = maxLength + 1
         
         return max(dp)
