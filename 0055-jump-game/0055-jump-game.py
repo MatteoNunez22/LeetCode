b7afacc87de8148685ff435f77729498beb50cfd
Matteo Nunez <matteo.nunez22@gmail.com>
2023-02-11 22:04:00 -0500

    Time: 503 ms (60.45%), Space: 15.4 MB (16.68%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,19 +1,27 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        n = len(nums)
-        dp = set()
+        # Slow Recursive + Memo: O(n^2), O(n)
+        # n = len(nums)
+        # dp = set()
+        # def dfs(i):
+        #     if i == n - 1:
+        #         return True
+        #     if i in dp or nums[i] == 0:
+        #         return False
+        #     for j in range(nums[i] + i, i, -1):
+        #         if j < n and dfs(j):
+        #             return True
+        #     dp.add(i)
+        #     return False
+        # return dfs(0)
         
-        def dfs(i):
-            if i == n - 1:
-                return True
+        # Greedy: O(n), O(1)
+        n = len(nums)
+        lastIdx = n - 1
+        
+        for i in range(n - 2, -1, -1):
+            if i + nums[i] >= lastIdx:
+                lastIdx = i
             
-            if i in dp or nums[i] == 0:
-                return False
-   
-            for j in range(nums[i] + i, i, -1):
-                if j < n and dfs(j):
-                    return True
-            dp.add(i)
-            return False
-            
-        return dfs(0)
+        return lastIdx == 0
+        
