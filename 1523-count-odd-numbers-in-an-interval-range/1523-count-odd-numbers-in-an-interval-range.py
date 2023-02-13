a397de6831ebe900db444862b948b5b364f52fe6
Matteo Nunez <matteo.nunez22@gmail.com>
2023-02-12 23:03:42 -0500

    Time: 28 ms (84.77%), Space: 13.8 MB (95.07%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
@@ -3,7 +3,9 @@ class Solution:
         
         l = (high - low + 1)
         
-        if l % 2 == 1 and low % 2 == 1:
+        if l % 2 == 0:
+            return l // 2
+        elif low % 2 == 1:
             return (l // 2) + 1
         else:
             return l // 2
