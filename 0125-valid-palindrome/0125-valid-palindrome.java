1c5ff7024468ee99c8dcf8fde2f6bc6658d273ef
Matteo Nunez <matteo.nunez22@gmail.com>
2023-03-29 23:35:01 -0400

    Time: 3 ms (97.42%), Space: 42.6 MB (64.92%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        for (int l = 0, r = s.length() - 1; l < r; l++, r--) {
+            while (l < r && !Character.isLetterOrDigit(s.charAt(l))) {
+                l++;
+            }
+            
+            while (l < r && !Character.isLetterOrDigit(s.charAt(r))) {
+                r--;
+            }
+            
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
