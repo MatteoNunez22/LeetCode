92ac47139f083d0a8a7a728bdc5b57a633d84633
Matteo Nunez <matteo.nunez22@gmail.com>
2023-04-02 14:14:23 -0400

    Time: 0 ms (100.00%), Space: 40.1 MB (5.08%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n < 2) return 1;
+        
+        int[] memo = new int[n+1];
+        Arrays.fill(memo, 0);
+        memo[0] = 1;
+        memo[1] = 1;
+        return recClimbStairs(n, memo);
+    }
+    
+    public int recClimbStairs(int n, int[] memo) {
+        if (memo[n] != 0) return memo[n];
+        
+        int ways = recClimbStairs(n-1, memo) + recClimbStairs(n-2, memo);
+        memo[n] = ways;
+        return ways;
+    }
+}
