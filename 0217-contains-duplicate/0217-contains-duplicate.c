8eb2f78e875727d4de2e99275a7aa981e6c682b4
Matteo Nunez <matteo.nunez22@gmail.com>
2023-04-13 00:55:44 -0400

    Time: 150 ms (31.27%), Space: 34.2 MB (6.07%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.c
+++ 0217-contains-duplicate/0217-contains-duplicate.c
@@ -1,37 +1,35 @@
 typedef struct {
-	int key;
-	UT_hash_handle hh; // Makes this structure hashable
+    int key;
+    UT_hash_handle hh;
 } hash_table;
 
 hash_table *hash = NULL, *elem, *tmp;
 
 bool containsDuplicate(int* nums, int numsSize){
     if (numsSize == 1) {
-      return false;
+        return false;
     }
     
     bool flag = false;
     
-    for (int i=0; i<numsSize; i++) {
+    for (int i = 0; i < numsSize; i++) {
         HASH_FIND_INT(hash, &nums[i], elem);
-        
-        if(!elem) {
-           elem = malloc(sizeof(hash_table));
-           elem->key = nums[i];
-           HASH_ADD_INT(hash, key, elem);
-            
-           flag = false;
-       }
-       else {
-           flag = true;
-           break;
-       }
+
+        if (!elem) {
+            elem = malloc(sizeof(hash_table));
+            elem->key = nums[i];
+            HASH_ADD_INT(hash, key, elem);
+            flag = false;
+        } else {
+            flag = true;
+            break;
+        }
     }
-                          
-    // Free up the hash table 
-	HASH_ITER(hh, hash, elem, tmp) {
-		HASH_DEL(hash, elem); free(elem);
-	}
+    
+    HASH_ITER(hh, hash, elem, tmp) {
+        HASH_DEL(hash, elem);
+        free(elem);
+    }
     
     return flag;
 }
