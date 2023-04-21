b5f017b18ab86d5a8a13ae4b239f2641af471c60
Matteo Nunez <matteo.nunez22@gmail.com>
2023-04-20 21:21:46 -0400

    Time: 35 ms (89.21%), Space: 7.4 MB (5.71%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.c
@@ -0,0 +1,21 @@
+int search(int* nums, int numsSize, int target){
+    
+    int l = 0;
+    int r = numsSize - 1;
+    int mid;
+    
+    while (l <= r) {
+        mid = floor((r + l) / 2);
+        if (nums[mid] < target) {
+            l = mid + 1;
+            
+        } else if (nums[mid] > target) {
+            r = mid - 1;
+            
+        } else {
+            return mid;
+        }
+    }
+    
+    return -1;
+}
